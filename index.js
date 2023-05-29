b6d23963caf20370e00a2d9abb20ea83c3727204
rubiyadav <rubiflyeis@gmail.com>
2023-05-29 18:55:51 +0530

    update

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const createError = require('http-errors');
 const express = require('express');
-const auth_Router = require('./router/authVender');
-const order_Router = require('./router/order_router');
-const funds = require('./router/funds');
-const contract_master = require('./router/conteact_master');
-const profile = require('./router/profile');
-const history = require('./router/history');
-const socket = require('./router/socket');
 const app = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -22,6 +15,13 @@ app.get('/', (req,res) =>{
         message: "Working"
     })
 })
+const auth_Router = require('./router/authVender');
+const order_Router = require('./router/order_router');
+const funds = require('./router/funds');
+const contract_master = require('./router/conteact_master');
+const profile = require('./router/profile');
+const history = require('./router/history');
+const socket = require('./router/socket');
 app.use('/api/v1/', auth_Router);
 app.use('/api/v1/order', order_Router);
 app.use('/api/v1/funds', funds);
@@ -65,7 +65,7 @@ mongoose.connection.on('disconnected', () => console.log('disconnected'));
 mongoose.connection.on('error', (error) => console.log(error));
 
 app.listen(process.env.PORT || 3001, async () => {
-    await mongoose.connect(process.env.DATABASE || "mongodb+srv://ankit:ankit@cluster0.vkljao5.mongodb.net/?retryWrites=true&w=majority", {
+    await mongoose.connect("mongodb+srv://ankit:ankit@cluster0.vkljao5.mongodb.net/?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
